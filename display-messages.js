d5397780caa1c5ea63ab325f07daa1b53149082d
Dibboraj <100348230+Dibboraj@users.noreply.github.com>
2023-10-31 19:53:31 +0600

    Update display-messages.js

--- display-messages.js
+++ display-messages.js
@@ -1,6 +1,7 @@
+// Create a reference to the chat-messages collection
 const chatMessagesRef = db.collection("chat-messages");
 
-// Order the chat messages by created_at in descending order
+// Create a query to get all chat messages, ordered by created_at in descending order
 const chatMessagesQuery = chatMessagesRef.orderBy("created_at", "desc");
 
 // Create an array to store the chat messages
@@ -8,57 +9,64 @@ const chatMessages = [];
 
 // Listen for new chat messages
 chatMessagesQuery.onSnapshot((snapshot) => {
-  // Get the changes that occurred since the last snapshot
-  const changes = snapshot.docChanges();
+// Get the changes that occurred since the last snapshot
+const changes = snapshot.docChanges();
 
-  // Loop through the changes
-  for (const change of changes) {
-    // Get the message document from the change
-    const message = change.doc;
+// Loop through the changes
+for (const change of changes) {
+// Get the message document from the change
+const message = change.doc;
 
-    // Check the type of the change
-    if (change.type === "added") {
-      // If a new message was added, insert it at the beginning of the chat messages array
-      chatMessages.unshift(message);
-    } else if (change.type === "modified") {
-      // If a message was modified, find its index in the chat messages array and replace it with the updated message
-      const index = chatMessages.findIndex((m) => m.id === message.id);
-      if (index !== -1) {
-        chatMessages[index] = message;
-      }
-    } else if (change.type === "removed") {
-      // If a message was removed, find its index in the chat messages array and remove it
-      const index = chatMessages.findIndex((m) => m.id === message.id);
-      if (index !== -1) {
-        chatMessages.splice(index, 1);
-      }
+// Check the type of the change
+switch (change.type) {
+  case "added":
+    // If a new message was added, insert it at the beginning of the chat messages array
+    chatMessages.unshift(message);
+    break;
+  case "modified":
+    // If a message was modified, find its index in the chat messages array and replace it with the updated message
+    const index = chatMessages.findIndex((m) => m.id === message.id);
+    if (index !== -1) {
+      chatMessages[index] = message;
     }
-  }
+    break;
+  case "removed":
+    // If a message was removed, find its index in the chat messages array and remove it
+    const index = chatMessages.findIndex((m) => m.id === message.id);
+    if (index !== -1) {
+      chatMessages.splice(index, 1);
+    }
+    break;
+  default:
+    // Do nothing if the change type is not recognized
+    break;
+}
+}
 
-  // Display the messages
-  displayMessages();
+// Display the messages
+displayMessages();
 });
 
 function displayMessages() {
-  // Clear the chat messages element
-  const chatMessagesElement = document.getElementById("chat-messages");
-  chatMessagesElement.innerHTML = "";
+// Clear the chat messages element
+const chatMessagesElement = document.getElementById("chat-messages");
+chatMessagesElement.innerHTML = "";
 
-  // Add the chat messages from the array to the chat messages element
-  for (const message of chatMessages) {
-    // Create a new chat message element
-    const chatMessageElement = document.createElement("div");
-    chatMessageElement.classList.add("chat-message");
+// Add the chat messages from the array to the chat messages element
+for (const message of chatMessages) {
+// Create a new chat message element
+const chatMessageElement = document.createElement("div");
+chatMessageElement.classList.add("chat-message");
 
-    // Set the chat message element's text
-    chatMessageElement.textContent = `${message.data().username}: ${message.data().message}`;
+// Set the chat message element's text
+chatMessageElement.textContent = `${message.data().username}: ${message.data().message}`;
 
-    // Add the chat message element to the chat messages element
-    chatMessagesElement.appendChild(chatMessageElement);
-  }
+// Add the chat message element to the chat messages element
+chatMessagesElement.appendChild(chatMessageElement);
+}
 
-  // Scroll to the bottom of the chat messages element
-  chatMessagesElement.scrollTop = chatMessagesElement.scrollHeight;
+// Scroll to the bottom of the chat messages element
+chatMessagesElement.scrollTop = chatMessagesElement.scrollHeight;
 }
 
 // Display the messages when the page loads
